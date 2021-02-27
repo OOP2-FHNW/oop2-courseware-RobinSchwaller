2d006c69cc6e5944b7eb3198791c1c93ec2d147b
RobinSchwaller <robinschwaller@gmail.com>
2021-02-27 14:03:45 +0100

    added HashSet

--- src/main/java/oop2/module02/set/Family.java
+++ src/main/java/oop2/module02/set/Family.java
@@ -1,10 +1,35 @@
 package oop2.module02.set;
 
+import oop2.module02.Person;
+
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * @author Dieter Holz
  */
 public class Family {
+    private final Set<Person> members = new HashSet<>();
 
+    public int size() {
+        return members.size();
+    }
 
+    public void add(Person person) {
+        members.add(person);
+    }
 
+    public boolean isMember(Person person) {
+        return members.contains(person);
+    }
+
+    public Person getOldest() {
+        Person oldest = null;
+        for(Person p: members){
+            if(oldest == null || p.getAge() > oldest.getAge()){
+                oldest = p;
+            }
+        }
+        return oldest;
+    }
 }
